686cd01b7750987efafdfb5e370063c1f8174ae1
Jean-Luc314 <47507431+Jean-Luc314@users.noreply.github.com>
2022-07-15 22:20:06 +0100

    Problem 4

--- Problem 004 - Largest palindrome product.jl
+++ Problem 004 - Largest palindrome product.jl
@@ -8,7 +8,6 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 # From largest palindrome downwards, check if product of two and stop if so
 
-largest = 999 * 999
 toDenary(N, pad = 1) = digits(N, base = 10, pad = pad) |> reverse
 
 function isPalindrome(N)
@@ -18,38 +17,33 @@ function isPalindrome(N)
     all(δ[i] == δ[n + 1 - i] for i in index)
 end
 
-function factor2(N, nDigits = 2, start = 2)
+function factor2(N::Integer, nDigits::Integer = 2, start::Integer = 2)
     limit = floor(Int, N ^ 0.5)
-    if (N % start == 0)
-        p = start
-        q = floor(Int, N / p)
-        testDigit(p) = (p >= 10 ^ (nDigits - 1)) & (p < 10 ^ nDigits)
-        if testDigit(p) & testDigit(q)
-            [p, q]
-        end
-    elseif start < limit
-        factor2(N, nDigits, start + 1)
-    else
+    if start > limit
         false
-    end
-end
-
-function getBiggestPalindrome(nDigits = 3)
-    cap = (10 ^ nDigits - 1) ^ 2
-    function searchPalindromes(start)
-        factors = factor2(start, nDigits)
-        if isPalindrome(start) & (factors isa Vector{Int64})
-            if all(length(x) == nDigits for x in factors)
-                Dict("p" => start, "f" => factors)
+    else
+        if N % start == 0
+            p = start
+            q = floor(Int, N / p)
+            testDigit(p) = (p >= 10 ^ (nDigits - 1)) & (p < 10 ^ nDigits)
+            if testDigit(p) & testDigit(q)
+                p, q
+            else
+                factor2(N, nDigits, start + 1)
             end
+        else
+            factor2(N, nDigits, start + 1)
         end
-        searchPalindromes(start - 1)
     end
-    searchPalindromes(cap)
 end
 
-getBiggestPalindrome(2)
+function largestPal(nDigits)
+    min = (10 ^ (nDigits - 1)) ^ 2
+    max = (10 ^ nDigits - 1) * 10 ^ nDigits
+    palindromes = filter(isPalindrome, min:max)
+    # Could have built the palindromes
+    p, q = last(filter(n -> n != false, map(n -> factor2(n, nDigits), palindromes)))
+    Dict("Palindrome" => p * q, "p" => p, "q" => q)
+end
 
-isPalindrome(91019)
-factor2(9009)
-(n + 1) % 2
+largestPal(3)
